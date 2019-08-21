3d8730743550eba735305a6b1a096b72a3ae4e0a
simon <eniacsimon@gmail.com>
2019-08-21 18:00:45 -0400

    update strategy_multidfs.py

--- templates/strategy_multidfs.py
+++ templates/strategy_multidfs.py
@@ -8,84 +8,92 @@ from socket import *
 import _pickle as pickle
 import threading
 
+isVisited = {}
+path = []
+isBack = False
+
+udpPort = 6666
+broadcastAddr = '10.0.0.255'
+bufferList = []
+
 class StrategyMultiDFS(Strategy):
-	mouse = None
-	isVisited = []
-	path = []
-	isBack = False
-
-	udpPort = 6666
-	broadcastAddr = '10.0.0.255'
-	bufferList = []
-
-	def __init__(self, mouse):
-		self.mouse = mouse
-		self.isVisited = [[0 for i in range(self.mouse.mazeMap.width)] for j in range(self.mouse.mazeMap.height)]
-		self.isVisited[self.mouse.x][self.mouse.y] = 1
-		self.socketUdp = socket(AF_INET, SOCK_DGRAM)
-		self.socketUdp.bind(('', self.udpPort))
-		self.socketUdp.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-		self.threadReceive = threading.Thread(name='receive', target=self.receiveDataThread)
-		self.threadReceive.setDaemon(True)
-		self.threadReceive.start()
+	def __init__(self, _mouse):
+		global mouse
+		mouse = _mouse
+		for i in range(mouse.mazeMap.width):
+			for j in range(mouse.mazeMap.height):
+				isVisited[(j, i)] = 0
+		isVisited[(mouse.x, mouse.y)] = 1
+		global socketUdp
+		socketUdp = socket(AF_INET, SOCK_DGRAM)
+		socketUdp.bind(('', udpPort))
+		socketUdp.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+		threadReceive = threading.Thread(name='receive', target=self.receiveDataThread)
+		threadReceive.setDaemon(True)
+		threadReceive.start()
 
 	def checkFinished(self):
-		return self.isBack
+		return isBack
 
 	def go(self):
-		self.mouse.senseWalls()
+		global mouse
+		mouse.senseWalls()
 		sendData = {}
-		sendData['x'] = self.mouse.x
-		sendData['y'] = self.mouse.y
-		sendData['up'] = not self.mouse.canGoUp()
-		sendData['down'] = not self.mouse.canGoDown()
-		sendData['left'] = not self.mouse.canGoLeft()
-		sendData['right'] = not self.mouse.canGoRight()
-		self.socketUdp.sendto(pickle.dumps(sendData), (self.broadcastAddr, self.udpPort))
+		sendData['x'] = mouse.x
+		sendData['y'] = mouse.y
+		sendData['up'] = not mouse.canGoUp()
+		sendData['down'] = not mouse.canGoDown()
+		sendData['left'] = not mouse.canGoLeft()
+		sendData['right'] = not mouse.canGoRight()
+		socketUdp.sendto(pickle.dumps(sendData), (broadcastAddr, udpPort))
 		# Update local map from the information sent by other robots
-		while len(self.bufferList) > 0:
-			recvData = pickle.loads(self.bufferList[0])
-			cell = self.mouse.mazeMap.getCell(recvData['x'], recvData['y'])
-			self.isVisited[recvData['x']][recvData['y']] = 1
-			if recvData['up']: self.mouse.mazeMap.setCellUpAsWall(cell)
-			if recvData['down']: self.mouse.mazeMap.setCellDownAsWall(cell)
-			if recvData['left']: self.mouse.mazeMap.setCellLeftAsWall(cell)
-			if recvData['right']: self.mouse.mazeMap.setCellRightAsWall(cell)
-			self.bufferList = self.bufferList[1:]
+		global bufferList
+		while len(bufferList) > 0:
+			recvData = pickle.loads(bufferList[0])
+			maze = mouse.mazeMap
+			cell = maze.getCell(recvData['x'], recvData['y'])
+			isVisited[(recvData['x'], recvData['y'])] = 1
+			if recvData['up']: maze.setCellUpAsWall(cell)
+			if recvData['down']: maze.setCellDownAsWall(cell)
+			if recvData['left']: maze.setCellLeftAsWall(cell)
+			if recvData['right']: maze.setCellRightAsWall(cell)
+			bufferList = bufferList[1:]
 
-		if self.mouse.canGoLeft() and not self.isVisited[self.mouse.x-1][self.mouse.y]:
-			self.path.append([self.mouse.x, self.mouse.y])
-			self.isVisited[self.mouse.x-1][self.mouse.y] = 1
-			self.mouse.goLeft()
-		elif self.mouse.canGoUp() and not self.isVisited[self.mouse.x][self.mouse.y-1]:
-			self.path.append([self.mouse.x, self.mouse.y])
-			self.isVisited[self.mouse.x][self.mouse.y-1] = 1
-			self.mouse.goUp()
-		elif self.mouse.canGoRight() and not self.isVisited[self.mouse.x+1][self.mouse.y]:
-			self.path.append([self.mouse.x, self.mouse.y])
-			self.isVisited[self.mouse.x+1][self.mouse.y] = 1
-			self.mouse.goRight()
-		elif self.mouse.canGoDown() and not self.isVisited[self.mouse.x][self.mouse.y+1]:
-			self.path.append([self.mouse.x, self.mouse.y])
-			self.isVisited[self.mouse.x][self.mouse.y+1] = 1
-			self.mouse.goDown()
+		if mouse.canGoLeft() and not isVisited[(mouse.x-1, mouse.y)]:
+			path.append([mouse.x, mouse.y])
+			isVisited[(mouse.x, mouse.y+1)] = 1
+			mouse.goLeft()
+		elif mouse.canGoUp() and not isVisited[(mouse.x, mouse.y-1)]:
+			path.append([mouse.x, mouse.y])
+			isVisited[(mouse.x, mouse.y+1)] = 1
+			mouse.goUp()
+		elif mouse.canGoRight() and not isVisited[(mouse.x+1, mouse.y)]:
+			path.append([mouse.x, mouse.y])
+			isVisited[(mouse.x, mouse.y+1)] = 1
+			mouse.goRight()
+		elif mouse.canGoDown() and not isVisited[(mouse.x, mouse.y+1)]:
+			path.append([mouse.x, mouse.y])
+			isVisited[(mouse.x, mouse.y+1)] = 1
+			mouse.goDown()
 		else:
-			if len(self.path) != 0:
-				x, y = self.path.pop()
-				if x < self.mouse.x:
-					self.mouse.goLeft()
-				elif x > self.mouse.x:
-					self.mouse.goRight()
-				elif y < self.mouse.y:
-					self.mouse.goUp()
-				elif y > self.mouse.y:
-					self.mouse.goDown()
+			if len(path) != 0:
+				x, y = path.pop()
+				if x < mouse.x:
+					mouse.goLeft()
+				elif x > mouse.x:
+					mouse.goRight()
+				elif y < mouse.y:
+					mouse.goUp()
+				elif y > mouse.y:
+					mouse.goDown()
 			else:
-				self.isBack = True
+				isBack = True
 
-		sleep(0.5)
+		sleep(0.2)
 
 	def receiveDataThread(self):
+		global socketUdp
+		global bufferList
 		while True:
-			dataRecv, addr = self.socketUdp.recvfrom(1000)
-			self.bufferList.append(dataRecv)
+			dataRecv, addr = socketUdp.recvfrom(1000)
+			bufferList.append(dataRecv)
